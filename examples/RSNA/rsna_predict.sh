5bd89fe6c30351ddfb47d927ba1960ad775e0344
Hengjian Jia <henryjia18@gmail.com>
2018-09-22 15:30:35 +0100

    Reorganise files and add in shell scripts for personal ease of use

---
+++ examples/RSNA/rsna_predict.sh
@@ -0,0 +1,1 @@
+python rsna_predict.py --root_dir /home/nvme/RSNAPneumonia --test_dir stage_1_test_images --sample_csv stage_1_sample_submission.csv --model RSNA_UNet.pth --out_csv submission.csv
---
+++ examples/RSNA/rsna_train.sh
@@ -0,0 +1,1 @@
+python rsna_train.py --root_dir /home/nvme/RSNAPneumonia --train_dir stage_1_train_images --train_csv stage_1_train_labels.csv

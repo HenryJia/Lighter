d4fce6e7ad0ece5f875ab977daac7cbfa242194b
Hengjian Jia <henryjia18@gmail.com>
2019-03-05 19:39:36 +0000

    Add option of using JSON config to specify parameters

--- examples/LJSpeech/spectrogram_run.py
+++ examples/LJSpeech/spectrogram_run.py
@@ -1,4 +1,4 @@
-import time, os, sys, argparse
+import time, os, sys, argparse, json
 import numpy as np
 import pandas as pd
 import scipy
@@ -27,14 +27,16 @@ from lighter.train.metrics import CategoricalAccuracy
 from lighter.models.model_lib.ljspeech import MelModel
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv_dir', required = True, type = str, help = 'Directory of training text dir')
-parser.add_argument('--audio_dir', required = True, type = str, help = 'Folder containing the training DICOM files')
+parser.add_argument('--json', type = str, help = 'JSON file to specify arguments and will overwrite any command line arguments')
+
+parser.add_argument('--csv_dir', type = str, help = 'Directory of training text dir')
+parser.add_argument('--audio_dir', type = str, help = 'Folder containing the training DICOM files')
 parser.add_argument('--sample_rate', default = 16000, type = int, help = 'Sample rate of data')
-parser.add_argument('--model_name', required = True, type = str, help = 'Output model name')
-parser.add_argument('--train_split', required = False, type = float, help = 'Proportion of the data used to train', default = 0.9)
-parser.add_argument('--use_amp', required = False, action = 'store_true', help = 'Whether to use NVidia automatic mixed precision.')
-parser.add_argument('--half', required = False, action = 'store_true', help = 'Whether to use half precision')
-parser.add_argument('--device', required = False, type = str, default = 'cuda:0', help = 'Which CUDA device to use')
+parser.add_argument('--model_name', type = str, help = 'Output model name')
+parser.add_argument('--train_split', type = float, help = 'Proportion of the data used to train', default = 0.9)
+parser.add_argument('--use_amp', action = 'store_true', help = 'Whether to use NVidia automatic mixed precision.')
+parser.add_argument('--half', action = 'store_true', help = 'Whether to use half precision')
+parser.add_argument('--device', type = str, default = 'cuda:0', help = 'Which CUDA device to use')
 
 parser.add_argument('--n_mels', default = 80, type = int, help = 'Number of mel filterbanks to use')
 parser.add_argument('--n_fft', default = 800, type = int, help = 'Size of fft window to use')
@@ -44,13 +46,20 @@ parser.add_argument('--stacks', default = 2, type = int, help = 'Number of WaveN
 parser.add_argument('--res_channels', default = 64, type = int, help = 'Number of WaveNet residual channel to use')
 parser.add_argument('--skip_channels', default = 256, type = int, help = 'Number of WaveNet skip channels to use')
 
-args = parser.parse_args()
+args, unknown_args = parser.parse_known_args()
 
-model = MelModel(n_mels = args.n_mels, n_fft = args.n_fft, hops = args.hops, depth = args.depth, stacks = args.stacks, res_channels = args.res_channels, skip_channels = args.skip_channels).to(torch.device(args.device))
-model.load_state_dict(torch.load(args.model_name))
+args = vars(args)
+if args['json'] is not None:
+    with open(args['json']) as f:
+        args_json = json.load(f)
+    args = {**args, **args_json}
+
+model = MelModel(n_mels = args['n_mels'], n_fft = args['n_fft'], hops = args['hops'], depth = args['depth'], stacks = args['stacks'], res_channels = args['res_channels'], skip_channels = args['skip_channels']).to(torch.device(args['device']))
+model.load_state_dict(torch.load(args['model_name']))
+model.eval()
 
 # Note, we could use torch.utils.rnn.pack_sequence instead of padding everything to a fixed length, but this is codewise easier for now
-mel_transform = Lambda(lambda x: melspectrogram(x, sr = args.sample_rate, n_mels = args.n_mels, n_fft = args.n_fft, hop_length = args.hops).astype(np.float32))
+mel_transform = Lambda(lambda x: melspectrogram(x, sr = args['sample_rate'], n_mels = args['n_mels'], n_fft = args['n_fft'], hop_length = args['hops']).astype(np.float32))
 log_transform = Lambda(lambda x: np.log(np.clip(x, a_min = 1e-5, a_max = None))) # Compress into Log-scale
 h_transforms = Compose([mel_transform, log_transform, Numpy2Tensor()])
 y_transforms = Compose([QuantiseULaw(u = 255), Lambda(lambda x: x.astype(np.long)), Numpy2Tensor()])
@@ -67,19 +76,19 @@ def joint_transform_f(x):
 joint_transforms = Lambda(joint_transform_f)
 
 # Create one dataset for everything and use PyTorch samplers to do the training/validation split
-data_set = LJSpeechDataset(args.csv_dir, args.audio_dir, text_transforms = None, audio_transforms = None, joint_transforms = joint_transforms, sample_rate = args.sample_rate)
+data_set = LJSpeechDataset(args['csv_dir'], args['audio_dir'], text_transforms = None, audio_transforms = None, joint_transforms = joint_transforms, sample_rate = args['sample_rate'])
 
 perm = np.random.permutation(len(data_set))
-train_sampler = SubsetRandomSampler(perm[:np.round(args.train_split * len(data_set)).astype(int)].tolist())
-validation_sampler = SubsetRandomSampler(perm[np.round(args.train_split * len(data_set)).astype(int):].tolist())
-train_loader = AsynchronousLoader(data_set, device = torch.device(args.device), batch_size = 1, shuffle = False, sampler = train_sampler)
-validation_loader = AsynchronousLoader(data_set, device = torch.device(args.device), batch_size = 1, shuffle = False, sampler = validation_sampler)
+train_sampler = SubsetRandomSampler(perm[:np.round(args['train_split'] * len(data_set)).astype(int)].tolist())
+validation_sampler = SubsetRandomSampler(perm[np.round(args['train_split'] * len(data_set)).astype(int):].tolist())
+train_loader = AsynchronousLoader(data_set, device = torch.device(args['device']), batch_size = 1, shuffle = False, sampler = train_sampler)
+validation_loader = AsynchronousLoader(data_set, device = torch.device(args['device']), batch_size = 1, shuffle = False, sampler = validation_sampler)
 
 
 for i, (x, y) in enumerate(validation_loader):
     with torch.no_grad():
         x = x[1]
-        out = torch.zeros((1, 1)).to(device = torch.device(args.device), dtype = torch.long)
+        out = torch.zeros((1, 1)).to(device = torch.device(args['device']), dtype = torch.long)
         bridge = model.bridge(x)
 
         pb = tqdm(total = bridge.shape[2])
@@ -116,11 +125,11 @@ out = expand(out)
 out = (out + 1.0) / 2.0 # rescale to [0.0, 1.0]
 out = out * (np.iinfo(np.int16).max - np.iinfo(np.int16).min) + np.iinfo(np.int16).min
 out = out.astype(np.int16)
-scipy.io.wavfile.write('test.wav', args.sample_rate, out)
+scipy.io.wavfile.write('test.wav', args['sample_rate'], out)
 
 y_wav = y[0].cpu().numpy().astype(np.float32)
 y_wav = expand(y_wav)
 y_wav = (y_wav + 1.0) / 2.0 # rescale to [0.0, 1.0]
 y_wav = y_wav * (np.iinfo(np.int16).max - np.iinfo(np.int16).min) + np.iinfo(np.int16).min
 y_wav = y_wav.astype(np.int16)
-scipy.io.wavfile.write('test_control.wav', args.sample_rate, y_wav)
+scipy.io.wavfile.write('test_control.wav', args['sample_rate'], y_wav)
--- examples/LJSpeech/spectrogram_train.py
+++ examples/LJSpeech/spectrogram_train.py
@@ -1,4 +1,4 @@
-import time, os, sys, argparse
+import time, os, sys, argparse, json
 import numpy as np
 import pandas as pd
 import scipy
@@ -27,16 +27,18 @@ from lighter.train.metrics import CategoricalAccuracy
 from lighter.models.model_lib.ljspeech import MelModel
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv_dir', required = True, type = str, help = 'Location of the metadata csv')
-parser.add_argument('--audio_dir', required = True, type = str, help = 'Folder containing the training audio files')
+parser.add_argument('--json', type = str, help = 'JSON file to specify arguments and will overwrite any command line arguments')
+
+parser.add_argument('--csv_dir', type = str, help = 'Location of the metadata csv')
+parser.add_argument('--audio_dir', type = str, help = 'Folder containing the training audio files')
 parser.add_argument('--sample_rate', default = 16000, type = int, help = 'Sample rate of data')
 parser.add_argument('--segment_length', default = 16000, type = int, help = 'Segment length of each piece of training data')
-parser.add_argument('--epochs', required = True, type = int, help = 'Number of epochs to train for')
-parser.add_argument('--batch_size', default = 16, type = int, help = 'Batch size')
-parser.add_argument('--model_name', required = True, type = str, help = 'Output model name')
-parser.add_argument('--train_split', required = False, type = float, help = 'Proportion of the data used to train', default = 0.9)
-parser.add_argument('--use_amp', required = False, action = 'store_true', help = 'Whether to use NVidia automatic mixed precision.')
-parser.add_argument('--half', required = False, action = 'store_true', help = 'Whether to use half precision')
+parser.add_argument('--epochs', type = int, help = 'Number of epochs to train for')
+parser.add_argument('--batch_size', default = 8, type = int, help = 'Batch size')
+parser.add_argument('--model_name', type = str, help = 'Output model name')
+parser.add_argument('--train_split', type = float, help = 'Proportion of the data used to train', default = 0.9)
+parser.add_argument('--use_amp', action = 'store_true', help = 'Whether to use NVidia automatic mixed precision.')
+parser.add_argument('--half', action = 'store_true', help = 'Whether to use half precision')
 parser.add_argument('--device', default = 'cuda:0', type = str, help = 'Which CUDA device to use')
 
 parser.add_argument('--n_mels', default = 80, type = int, help = 'Number of mel filterbanks to use')
@@ -47,18 +49,24 @@ parser.add_argument('--stacks', default = 2, type = int, help = 'Number of WaveN
 parser.add_argument('--res_channels', default = 64, type = int, help = 'Number of WaveNet residual channel to use')
 parser.add_argument('--skip_channels', default = 256, type = int, help = 'Number of WaveNet skip channels to use')
 
-args = parser.parse_args()
+args, unknown_args = parser.parse_known_args()
 
-model = MelModel(n_mels = args.n_mels, n_fft = args.n_fft, hops = args.hops, depth = args.depth, stacks = args.stacks, res_channels = args.res_channels, skip_channels = args.skip_channels).to(torch.device(args.device))
+args = vars(args)
+if args['json'] is not None:
+    with open(args['json']) as f:
+        args_json = json.load(f)
+    args = {**args, **args_json}
+
+model = MelModel(n_mels = args['n_mels'], n_fft = args['n_fft'], hops = args['hops'], depth = args['depth'], stacks = args['stacks'], res_channels = args['res_channels'], skip_channels = args['skip_channels']).to(torch.device(args['device']))
 
 # Note, we could use torch.utils.rnn.pack_sequence instead of padding everything to a fixed length, but this is codewise easier for now
-mel_transform = Lambda(lambda x: melspectrogram(x, sr = args.sample_rate, n_mels = args.n_mels, n_fft = args.n_fft, hop_length = args.hops).astype(np.float32))
+mel_transform = Lambda(lambda x: melspectrogram(x, sr = args['sample_rate'], n_mels = args['n_mels'], n_fft = args['n_fft'], hop_length = args['hops']).astype(np.float32))
 log_transform = Lambda(lambda x: np.log(np.clip(x, a_min = 1e-5, a_max = None))) # Compress into Log-scale
 h_transforms = Compose([mel_transform, log_transform, Numpy2Tensor()])
 y_transforms = Compose([QuantiseULaw(u = 255), Lambda(lambda x: x.astype(np.long)), Numpy2Tensor()])
 
-#seq_sampler = SampleSequence1D(length = model.wavenet.get_receptive_field() + args.segment_length, dim = 0)
-seq_sampler = SampleSequence1D(length = args.segment_length, dim = 0)
+#seq_sampler = SampleSequence1D(length = model.wavenet.get_receptive_field() + args['segment_length'], dim = 0)
+seq_sampler = SampleSequence1D(length = args['segment_length'], dim = 0)
 
 def joint_transform_f(x):
     x = seq_sampler(x[1])
@@ -72,15 +80,15 @@ def joint_transform_f(x):
 joint_transforms = Lambda(joint_transform_f)
 
 # Create one dataset for everything and use PyTorch samplers to do the training/validation split
-data_set = LJSpeechDataset(args.csv_dir, args.audio_dir, text_transforms = None, audio_transforms = None, joint_transforms = joint_transforms, sample_rate = args.sample_rate)
+data_set = LJSpeechDataset(args['csv_dir'], args['audio_dir'], text_transforms = None, audio_transforms = None, joint_transforms = joint_transforms, sample_rate = args['sample_rate'])
 
 perm = np.random.permutation(len(data_set))
-train_sampler = SubsetRandomSampler(perm[:np.round(args.train_split * len(data_set)).astype(int)].tolist())
-validation_sampler = SubsetRandomSampler(perm[np.round(args.train_split * len(data_set)).astype(int):].tolist())
-train_loader = AsynchronousLoader(data_set, device = torch.device(args.device), batch_size = args.batch_size, shuffle = False, sampler = train_sampler)
-validation_loader = AsynchronousLoader(data_set, device = torch.device(args.device), batch_size = args.batch_size, shuffle = False, sampler = validation_sampler)
+train_sampler = SubsetRandomSampler(perm[:np.round(args['train_split'] * len(data_set)).astype(int)].tolist())
+validation_sampler = SubsetRandomSampler(perm[np.round(args['train_split'] * len(data_set)).astype(int):].tolist())
+train_loader = AsynchronousLoader(data_set, device = torch.device(args['device']), batch_size = args['batch_size'], shuffle = False, sampler = train_sampler)
+validation_loader = AsynchronousLoader(data_set, device = torch.device(args['device']), batch_size = args['batch_size'], shuffle = False, sampler = validation_sampler)
 
-if args.half:
+if args['half']:
     model = model.half()
     optim = Adam(model.parameters(), lr = 1e-3, eps = 1e-4)
 else:
@@ -89,17 +97,17 @@ else:
 loss = [nn.CrossEntropyLoss().cuda()]
 metrics = [(0, CategoricalAccuracy().cuda())]
 
-train_step = DefaultStep(model = model, losses = loss, optimizer = optim, metrics = metrics, use_amp = args.use_amp)
-validation_step = DefaultStep(model = model, losses = loss, optimizer = optim, metrics = metrics, use_amp = args.use_amp)
+train_step = DefaultStep(model = model, losses = loss, optimizer = optim, metrics = metrics, use_amp = args['use_amp'])
+validation_step = DefaultStep(model = model, losses = loss, optimizer = optim, metrics = metrics, use_amp = args['use_amp'])
 
 train_callbacks = [ProgBarCallback(check_queue = True)]
-validation_callback = train_callbacks + [CheckpointCallback(args.model_name, monitor = 'CategoricalAccuracy_0', save_best = True, mode = 'max')]
+validation_callback = train_callbacks + [CheckpointCallback(args['model_name'], monitor = 'CategoricalAccuracy_0', save_best = True, mode = 'max')]
 
 trainer = Trainer(train_loader, train_step, train_callbacks)
 validator = Trainer(validation_loader, validation_step, validation_callback)
 
 
-for i in range(args.epochs):
+for i in range(args['epochs']):
     model.train()
     print('Training Epoch {}'.format(i))
     next(trainer)

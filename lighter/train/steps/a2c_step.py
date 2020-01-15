210cb5c8dfaa6457935509de1001de4e4b4eabf3
Hengjian Jia <henryjia18@gmail.com>
2020-01-15 13:06:43 +0000

    Change behaviour to finish after all concurrent episodes are finished

--- lighter/train/steps/a2c_step.py
+++ lighter/train/steps/a2c_step.py
@@ -42,25 +42,33 @@ class A2CStep(object):
     use_amp: Boolean
         Whether to use NVidia's automatic mixed precision training
     """
-    def __init__(self, env, agent, optimizer, update_interval=0, gamma=0.9, epsilon=1e-5, metrics=[], train=True, use_amp=False):
+    def __init__(self, env, agent, optimizer, update_interval=0, gamma=0.9, entropy_weight=1e-4, epsilon=1e-5, metrics=[], train=True, use_amp=False):
         self.env = env if type(env) is list else [env]
         self.agent = agent
         self.update_interval = update_interval
         self.optimizer = optimizer
         self.gamma = gamma
+        self.entropy_weight = entropy_weight
         self.epsilon = epsilon
         self.metrics = metrics
         self.train = train
         self.use_amp = use_amp
 
         self.probs = []
-        self.log_probs = [] # Note this is the log likelihood of the actions, couldn't think of a better name
+        self.log_probs = []
+        self.done_history = [[False] * len(self.env)] # We can't be done on the initial state
         self.value_history = []
         self.reward_history = []
 
 
     def reset(self): # Reset our environment back to the beginning
         self.states = [e.reset() for e in self.env]
+
+        self.probs = []
+        self.log_probs = []
+        self.done_history = [[False] * len(self.env)]
+        self.value_history = []
+        self.reward_history = []
 
 
     def __call__(self): # We don't actually need any data since the environment is a member variable
@@ -72,23 +80,28 @@ class A2CStep(object):
         out, value = self.agent(states)
         actions = distributions.Categorical(out).sample().tolist()
 
-        env_out = [e.step(a) for a, e in zip(actions, self.env)]
+        env_out = []
+        for a, e, d in zip(actions, self.env, self.done_history[-1]):
+            if not d: # Continue the envs that are not done yet
+                env_out += [e.step(a)]
+            else: # Otherwise stop and fill the entries with appropriate values
+                env_out += [[np.zeros(states[0].shape), 0, True, None]]
+
         next_states, rewards, done, info = list(zip(*env_out)) # Use the zip transposition trick
         self.states = next_states
 
         metrics = [('reward', np.mean(rewards))]
-        done = any(done) # We're done if any of the environments are done
 
         if self.train:
             self.probs.append(out)
             self.log_probs.append(torch.log(out[torch.arange(len(actions)), actions]))
             self.value_history.append(value[:, 0])
 
-            #reward = torch.tensor(reward).to(device=device, dtype=torch.float32, non_blocking=True)
-            # Keep rewards out of the GPU, we have to loop through and CPU ops should be a little faster for now
+            self.done_history.append(done)
+            # Keep rewards out of the GPU, we have to loop through and CPU ops should be a little faster
             self.reward_history.append(torch.tensor(rewards))
 
-            if done or len(self.probs) == self.update_interval:
+            if all(done) or len(self.probs) == self.update_interval:
                 returns = []
                 R = 0
                 # Compute the return at each time step with discount factor
@@ -99,21 +112,27 @@ class A2CStep(object):
 
                 # Time dimension across dimension 0, concurrency/batch dimension across dimension 1
                 returns = torch.stack(returns, dim=0).to(device=device, dtype=torch.float32, non_blocking=True)
-                returns = (returns - returns.mean(dim=0, keepdims=True))
+
+                # Note: the indexing shifts it by one so we're not masking out the final state as the gym env will return done on the final state
+                done_mask = 1 - torch.tensor(self.done_history[:-1]).to(device=device, dtype=torch.float32, non_blocking=True)
+
+                # We have to manually compute mean and std since we need to mask the envs which are done
+                returns = (returns - torch.sum(returns * done_mask, dim=0) / (torch.sum(done_mask, dim=0) + self.epsilon))
                 if returns.shape[0] > 1: # PyTorch returns NaN if you try taking std of an array of size 1
-                    returns /= (returns.std(dim=0, keepdims=True) + self.epsilon)
+                    returns /= (torch.sum(returns ** 2 * done_mask, dim=0) / (torch.sum(done_mask, dim=0) - 1 + self.epsilon) + self.epsilon)
 
                 value = torch.stack(self.value_history, dim=0).detach()
 
-                advantage = returns - value
+                advantage = done_mask * (returns - value)
 
                 log_probs = torch.stack(self.log_probs, dim=0)
                 probs = torch.stack(self.probs, dim=0)
 
                 policy_loss = -torch.sum(log_probs * advantage)
-                value_loss = F.smooth_l1_loss(value, returns)
-                entropy_loss = -torch.mean(torch.sum(probs * torch.log(probs), dim=-1))
-                loss = policy_loss + value_loss - 1e-4 * entropy_loss
+                value_loss = F.smooth_l1_loss(done_mask * value, done_mask * returns)
+                entropy_loss = -torch.mean(done_mask * torch.sum(probs * torch.log(probs), dim=-1)) # actions dimension will be the last one
+                loss = policy_loss + value_loss - self.entropy_weight * entropy_loss
+
                 self.optimizer.zero_grad()
                 if self.use_amp:
                     with amp.scale_loss(loss, self.optimizer) as scaled_loss:
@@ -129,10 +148,11 @@ class A2CStep(object):
 
                 self.probs = []
                 self.log_probs = []
+                self.done_history = [self.done_history[-1]] # Keep the last bit so we still know if the environemtn is done
                 self.value_history = []
                 self.reward_history = []
 
-                return StepReport(outputs = {'out': out.detach(), 'action': actions}, losses={'loss': loss.item()}, metrics=dict(metrics)), done
+                return StepReport(outputs = {'out': out.detach(), 'action': actions}, losses={'loss': loss.item()}, metrics=dict(metrics)), all(done)
 
-        return StepReport(outputs = {'out': out.detach(), 'action': actions}, losses={'loss': 0}, metrics=dict(metrics)), done
+        return StepReport(outputs = {'out': out.detach(), 'action': actions}, losses={'loss': 0}, metrics=dict(metrics)), all(done)
 
